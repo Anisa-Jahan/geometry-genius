97d766449aafccd60e1e2e571123cbd7455b8306
Anisa-Jahan <anisajahan3121@gmail.com>
2024-10-17 09:34:24 +0600

    done total calculation of 1st three item

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -6,8 +6,10 @@ function calculateTriangleArea(){
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
+    const triangleArae = 0.5 * base * height;
     console.log(typeof base, typeof height);
     console.log(base, height);
+    console.log(triangleArae);
 }
 
 function calculateRectangleArae(){
@@ -18,6 +20,22 @@ function calculateRectangleArae(){
     const rectangleLengthInput = document.getElementById('rectangle-length');
     const rectangleLengthText = rectangleLengthInput.value;
     const length = parseFloat(rectangleLengthText);
+    const rectangleArea = width * length;
     console.log(typeof width, typeof length);
     console.log(width, length);
+    console.log(rectangleArea);
+}
+
+function calculateParallelogramArea(){
+    const parllelolgramBaseInput = document.getElementById('parallelogram-base');
+    const parllelolgramBaseText = parllelolgramBaseInput.value
+    const base = parseFloat(parllelolgramBaseText);
+     
+    const parllelolgramHeightInput = document.getElementById('parallelogram-height');
+    const parllelolgramHeightText = parllelolgramHeightInput.value
+    const height = parseFloat(parllelolgramHeightText);
+    const parallelogramArea = base * height;
+    console.log(typeof base, typeof height);
+    console.log(base, height);
+    console.log(parallelogramArea);
 }
