a2c32a10a32477e3c4b2ef62c79e5102f9ebde90
Anisa-Jahan <anisajahan3121@gmail.com>
2024-10-19 09:20:11 +0600

    calculation of every element and display in card were done

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -7,9 +7,12 @@ function calculateTriangleArea(){
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
     const triangleArae = 0.5 * base * height;
-    console.log(typeof base, typeof height);
-    console.log(base, height);
-    console.log(triangleArae);
+
+    // console.log(typeof base, typeof height);
+    // console.log(base, height);
+    // console.log(triangleArae);
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = triangleArae;
 }
 
 function calculateRectangleArae(){
@@ -21,9 +24,12 @@ function calculateRectangleArae(){
     const rectangleLengthText = rectangleLengthInput.value;
     const length = parseFloat(rectangleLengthText);
     const rectangleArea = width * length;
-    console.log(typeof width, typeof length);
-    console.log(width, length);
-    console.log(rectangleArea);
+
+    // console.log(typeof width, typeof length);
+    // console.log(width, length);
+    // console.log(rectangleArea);
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = rectangleArea;
 }
 
 function calculateParallelogramArea(){
@@ -35,7 +41,61 @@ function calculateParallelogramArea(){
     const parllelolgramHeightText = parllelolgramHeightInput.value
     const height = parseFloat(parllelolgramHeightText);
     const parallelogramArea = base * height;
-    console.log(typeof base, typeof height);
-    console.log(base, height);
-    console.log(parallelogramArea);
+
+    // console.log(typeof base, typeof height);
+    // console.log(base, height);
+    // console.log(parallelogramArea);
+    const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+    parallelogramAreaSpan.innerText = parallelogramArea;
+}
+
+function calculateRhombusArea(){
+    const rhombusFirstDiagonalInput = document.getElementById('diagonal-1');
+    const rhombusFirstDiagonalText = rhombusFirstDiagonalInput.value;
+    const FirstDiagonal = parseFloat(rhombusFirstDiagonalText);
+
+    const rhombusSecondDiagonalInput = document.getElementById('diagonal-2');
+    const rhombusSecondDiagonalText = rhombusSecondDiagonalInput.value;
+    const SecondDiagonal = parseFloat(rhombusSecondDiagonalText);
+    const rhombusArea = 0.5*FirstDiagonal*SecondDiagonal;
+
+    // console.log(typeof FirstDiagonal, typeof SecondDiagonal);
+    // console.log(FirstDiagonal, SecondDiagonal);
+    // console.log(rhombusArea);
+    const rhombusAreaSpan = document.getElementById('rhombus-area');
+    rhombusAreaSpan.innerText = rhombusArea;
+}
+
+function calculatePentagonArae(){
+    const pentagonPerimeterInput = document.getElementById('periemeter');
+    const pentagonPerimeterText = pentagonPerimeterInput.value;
+    const perimeter = parseFloat(pentagonPerimeterText);
+
+    const pentagonApothemInput = document.getElementById('apothem');
+    const pentagonApothemText = pentagonApothemInput.value;
+    const apothem = parseFloat(pentagonApothemText);
+    const pentagonArea = 0.5*perimeter*apothem;
+
+    // console.log(typeof perimeter, typeof apothem);
+    // console.log(perimeter, apothem);
+    // console.log(pentagonArea);
+    const pentagonAreaSpan = document.getElementById('pentagon-area');
+    pentagonAreaSpan.innerText = pentagonArea;
+}
+
+function calculateEllipseArea(){
+    const ellipseMajorAxisInput = document.getElementById('major-axis');
+    const ellipseMajorAxisText = ellipseMajorAxisInput.value;
+    const majorAxis = parseFloat(ellipseMajorAxisText);
+
+    const ellipseMinorAxisInput = document.getElementById('minor-axis');
+    const ellipseMinorAxisText = ellipseMinorAxisInput.value;
+    const minorAxis = parseFloat(ellipseMinorAxisText);
+    const ellipseArea = 3.1416*majorAxis*minorAxis;
+
+    // console.log(typeof majorAxis, typeof minorAxis);
+    // console.log(majorAxis, minorAxis);
+    // console.log(ellipseArea);
+    const ellipseAreaSpan = document.getElementById('ellipse-area');
+    ellipseAreaSpan.innerText = ellipseArea;
 }
